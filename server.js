1b46cc51f8c6fbb20681da4b559d7d0bef0d965c
Resham Rout <reshamrout@gmail.com>
2025-10-31 23:10:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,15 +9,8 @@ const bookingsRoutes = require('./routes/bookings');
 const promoRoutes = require('./routes/promo');
 
 const app = express();
-app.use(cors({
-  origin: [
-    'https://book-it-backend-2pjnpy5ar-resham-routs-projects.vercel.app', // your deployed frontend
-    'http://localhost:5173'                // local development
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(cors());
+
 app.use(express.json());
 app.use(morgan('dev'));
 
