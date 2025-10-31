ad7157123bf26045f816df2ac90d2f686d38592c
Resham Rout <reshamrout@gmail.com>
2025-10-31 23:46:36 +0530

    Fixed cors error

--- server.js
+++ server.js
@@ -14,13 +14,23 @@ const allowedOrigins = [
   'https://book-it-frontend-l7ai3af0q-resham-routs-projects.vercel.app'
 ];
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  })
-);
+// Explicit CORS middleware
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  // Handle preflight request
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use(morgan('dev'));
